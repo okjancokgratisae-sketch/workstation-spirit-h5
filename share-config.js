19b632267811c9e55bc69d494444a93f7bc8677d
郭昱 <420166401@qq.com>
2026-06-15 18:49:22 +0800

    Improve precision of workstation spirit test

--- share-config.js
+++ share-config.js
@@ -1,6 +1,6 @@
 window.H5_SHARE_CONFIG = {
   title: "测测你的工位精神体：公司越疯，你越像谁？",
-  desc: "公司副本一开，精神值就开始漏电。20 题检测你的工位精神体会觉醒成什么形态。",
+  desc: "20 题检测你的工位精神体：公司越疯，你越会启动哪种自救程序？",
   image: "./assets/share_cover.png",
   assetBase: "./assets"
 };
